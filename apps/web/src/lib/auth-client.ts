89c2b46fc86a35103e12e56e26a3c71eb8c28756
Prakash-Raut <prakash_raut@outlook.com>
2025-12-09 17:16:45 +0530

    refactor:  better-auth-client

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,9 +1,11 @@
+import type { auth } from "@better-uptime/auth";
 import { polarClient } from "@polar-sh/better-auth";
+import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
 	baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
-	plugins: [polarClient()],
+	plugins: [polarClient(), inferAdditionalFields<typeof auth>()],
 	fetchOptions: {
 		credentials: "include",
 	},
